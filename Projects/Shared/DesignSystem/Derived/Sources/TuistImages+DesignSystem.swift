724fa021c11e53b779332b2ba234eadcf7795160
khyeji98 <gpwl98wkd@gmail.com>
2026-05-16 00:11:49 +0900

    [CHORE]: DesignSystem에 소셜 로그인 심볼 이미지 에셋 추가

--- Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
+++ Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
@@ -6,6 +6,9 @@ import SwiftUI
 
 public extension Image {
   enum Asset {
+    public static let symApple: Image = Image("sym_apple", bundle: Bundle.module)
+    public static let symKakao: Image = Image("sym_kakao", bundle: Bundle.module)
+    public static let symNaver: Image = Image("sym_naver", bundle: Bundle.module)
   }
 }
 #endif
