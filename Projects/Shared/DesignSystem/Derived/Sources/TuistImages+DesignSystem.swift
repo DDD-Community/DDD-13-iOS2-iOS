db177ebd2d0c3a1d3e65b0363aab389f31a699ce
khyeji98 <gpwl98wkd@gmail.com>
2026-05-16 05:19:39 +0900

    [CHORE]: DesignSystem에 TextButton용 화살표 아이콘 에셋 추가

--- Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
+++ Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
@@ -6,6 +6,8 @@ import SwiftUI
 
 public extension Image {
   enum Asset {
+    public static let icArrowRight24: Image = Image("ic_arrow_right_24", bundle: Bundle.module)
+    public static let icArrowRightDisabled24: Image = Image("ic_arrow_right_disabled_24", bundle: Bundle.module)
     public static let symApple: Image = Image("sym_apple", bundle: Bundle.module)
     public static let symKakao: Image = Image("sym_kakao", bundle: Bundle.module)
     public static let symNaver: Image = Image("sym_naver", bundle: Bundle.module)
