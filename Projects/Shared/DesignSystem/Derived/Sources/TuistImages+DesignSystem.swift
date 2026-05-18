2cb6cff69cbd31e0db77ace5be7336348e751dac
khyeji98 <gpwl98wkd@gmail.com>
2026-05-18 18:26:52 +0900

    [CHORE]: 신규 컴포넌트용 이미지 에셋 추가

--- Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
+++ Projects/Shared/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
@@ -6,8 +6,19 @@ import SwiftUI
 
 public extension Image {
   enum Asset {
+    public static let icArrowDown24: Image = Image("ic_arrow_down_24", bundle: Bundle.module)
     public static let icArrowRight24: Image = Image("ic_arrow_right_24", bundle: Bundle.module)
     public static let icArrowRightDisabled24: Image = Image("ic_arrow_right_disabled_24", bundle: Bundle.module)
+    public static let icArrowUp24: Image = Image("ic_arrow_up_24", bundle: Bundle.module)
+    public static let icCheckboxCircleDisabledMd: Image = Image("ic_checkbox_circle_disabled_md", bundle: Bundle.module)
+    public static let icCheckboxCircleDisabledSm: Image = Image("ic_checkbox_circle_disabled_sm", bundle: Bundle.module)
+    public static let icCheckboxCircleEnabledMd: Image = Image("ic_checkbox_circle_enabled_md", bundle: Bundle.module)
+    public static let icCheckboxCircleEnabledSm: Image = Image("ic_checkbox_circle_enabled_sm", bundle: Bundle.module)
+    public static let icCheckboxGhostDisabledMd: Image = Image("ic_checkbox_ghost_disabled_md", bundle: Bundle.module)
+    public static let icCheckboxGhostDisabledSm: Image = Image("ic_checkbox_ghost_disabled_sm", bundle: Bundle.module)
+    public static let icCheckboxGhostEnabledMd: Image = Image("ic_checkbox_ghost_enabled_md", bundle: Bundle.module)
+    public static let icCheckboxGhostEnabledSm: Image = Image("ic_checkbox_ghost_enabled_sm", bundle: Bundle.module)
+    public static let icCircleQuestionFilled24: Image = Image("ic_circle_question_filled_24", bundle: Bundle.module)
     public static let symApple: Image = Image("sym_apple", bundle: Bundle.module)
     public static let symKakao: Image = Image("sym_kakao", bundle: Bundle.module)
     public static let symNaver: Image = Image("sym_naver", bundle: Bundle.module)
